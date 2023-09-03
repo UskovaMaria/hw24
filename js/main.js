b644c1d2f01e3f8226f084d231b63671098f3b67
Uskova Maria <uskovamaria16@gmail.com>
2023-09-03 15:00:13 +0200

    number

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+let a = 5;
+let b = 7;
+let c = 2;
+
+let sum = a + b;
+console.log(sum);
+
+let difference = a - b;
+console.log(difference);
+
+let multiplication = a * b;
+console.log(multiplication);
+
+let division = b / a;
+console.log(division);
+
+let degrees = Math.pow(a, c);
+console.log(degrees);
+
+let percentages = 5 * 100 / 7;
+console.log(percentages);
+
+
